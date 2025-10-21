3fc5c516e997fc43b38147a4176412340d4a0df7
betuah <betuahanugerah@gmail.com>
2025-10-21 14:13:01 +0700

    chore: update gitignore

--- database/scripts/backup.sh
+++ database/scripts/backup.sh
@@ -6,17 +6,17 @@ export PGPASSWORD="${POSTGRES_PASSWORD}"
 
 # Timestamp folder
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-BACKUP_DIR=/dumps/$TIMESTAMP
+BACKUP_DIR="/dumps/$TIMESTAMP"
 mkdir -p "$BACKUP_DIR"
 
 echo "📦 Dumping globals (roles, permissions)..."
 pg_dumpall -h postgres -U "${DB_USER}" --globals-only > "$BACKUP_DIR/globals.sql"
 
 echo "📦 Dumping databases..."
-IFS=',' read -ra DBS <<< "$DB_BACKUP_NAME"
+IFS=',' read -ra DBS <<< "${DB_BACKUP_NAME}"
 for db in "${DBS[@]}"; do
-  echo "Dumping database: $db"
-  pg_dump -h postgres -U "${DB_USER}" -Fc -f "$BACKUP_DIR/$db.dump" "$db"
+    echo "Dumping database: $db"
+    pg_dump -h postgres -U "${DB_USER}" -Fc -f "$BACKUP_DIR/$db.dump" "$db"
 done
 
 echo "✅ Backup completed: $BACKUP_DIR"
